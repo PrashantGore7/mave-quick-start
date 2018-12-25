2271d979af16fb3cae7735bb276e2585d58e34fd
PrashantGore7 <prashantgore.prash@gmail.com>
2018-12-25 15:11:12 +0000

    Adding a source file to the git repository

---
+++ src/main/java/com/spring/apacheCamel/demo/Sample.java
@@ -0,0 +1,17 @@
+package com.spring.apacheCamel.demo;
+
+public class Sample{
+	
+	public Sample(){
+		
+		System.out.println("Inside Application");
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Starting Application");
+		
+		Sample sample= new Sample();
+	}
+
+}
